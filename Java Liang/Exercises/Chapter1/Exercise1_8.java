b14ef42630cddc17570a49be507e89f8048e8ead
Steven Gonzalez <steven.gonzalez3751@campus.mccd.edu>
2018-02-18 12:33:06 -0800

    Create Exercise1_8.java

---
+++ Java Liang/Exercises/Chapter1/Exercise1_8.java
@@ -0,0 +1,10 @@
+public class Exercise1_8 {
+    public static void main(String[] args) {
+        int radius  = 2;
+        double pi = 3.1415;
+        double perimeter = 2*pi*radius;
+        double area = pi*radius*radius;
+        System.out.println("Perimeter is " + perimeter);
+        System.out.println("Area is " + area);
+    }
+}
