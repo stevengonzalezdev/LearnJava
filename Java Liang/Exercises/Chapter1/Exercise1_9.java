269e0632de66f143e55009ed5003c959fda43041
Steven Gonzalez <steven.gonzalez3751@campus.mccd.edu>
2018-02-18 12:55:02 -0800

    Create Exercise1_9.java

---
+++ Java Liang/Exercises/Chapter1/Exercise1_9.java
@@ -0,0 +1,10 @@
+public class Exercise1_9 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+        double area = width*height;
+        double perimeter = 2*width + 2*height;
+        System.out.println("The Perimeter is " + perimeter);
+        System.out.println("The area is " + area );
+    }
+}
