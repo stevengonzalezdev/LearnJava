856a58a0a1898792ab19d91665704eda5f3fc895
Steven Gonzalez <steven.gonzalez3751@campus.mccd.edu>
2018-02-18 10:45:26 -0800

    Create Exercise1_3.java

---
+++ Java Liang/Exercises/Chapter1/Exercise1_3.java
@@ -0,0 +1,9 @@
+
+public class exercise {
+    public static void main(String[] args) {
+        System.out.println("    j    A   V     V    A");
+        System.out.println("    j   A A   V   V    A A");
+        System.out.println("j   j  AAAAA   V V    AAAAA");
+        System.out.println("  j   A     A   V    A     A");
+    }
+}
